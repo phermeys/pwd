a64f4e6b4b8d1ff84af26ecb6b1bb420d2573984
phermeys <matt@theregion.beer>
2023-12-17 17:32:16 +0000

    Adding OG Source Code

---
+++ rand.php
@@ -0,0 +1,10 @@
+
+<?php
+echo rand(1,70)."<br>";
+echo rand(1,70)."<br>";
+echo rand(1,70)."<br>";
+echo rand(1,70)."<br>";
+echo rand(1,70)."<br>";
+echo rand(1,70)."<br>";
+echo rand(1,70)."<br>";
+?>
